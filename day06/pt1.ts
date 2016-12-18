d8dd180b969879326a309805e31ec0a3de0ae006
Tom Fenech <tomjwfenech@gmail.com>
2016-12-18 18:55:35 +0100

    Day 6

---
+++ day06/pt1.ts
@@ -0,0 +1,37 @@
+import { input } from "./input";
+
+let allCounts: { [char: string]: number }[];
+input.split("\n").forEach((line, index) => {
+    const chars = line.split("");
+    if (index === 0) {
+        allCounts = chars.map(() => ({}));
+    }
+    line.split("").forEach((char, index) => {
+        const counts = allCounts[index];
+        if (char in counts) {
+            ++counts[char]
+        } else {
+            counts[char] = 1;
+        }
+    });
+});
+
+const message = allCounts.reduce((letters, counts) => {
+    const letter = Object.keys(counts).reduce((prev, curr) => {
+        const count = counts[curr];
+        if (count > prev.count) {
+            return {
+                letter: curr,
+                count
+            };
+        }
+        return prev;
+    }, { letter: <string>null, count: 0 });
+
+    return [
+        ...letters,
+        letter.letter
+    ];
+}, []).join("");
+
+console.log(message);
