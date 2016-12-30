f47a29e320e60746bc07524c88512a311dfc2559
Tom Fenech <tomjwfenech@gmail.com>
2016-12-30 02:42:46 +0100

    Day 13

---
+++ day13/favouriteNumber.ts
@@ -0,0 +1,1 @@
+export const favouriteNumber = 1364;
---
+++ day13/pt1.ts
@@ -0,0 +1,110 @@
+import { favouriteNumber } from "./favouriteNumber";
+
+enum Type {
+    SPACE,
+    WALL
+}
+
+interface Coords {
+    x: number;
+    y: number;
+}
+
+interface State {
+    coords: Coords;
+    steps: number;
+}
+
+let state = {
+    coords: {
+        x: 1,
+        y: 1
+    },
+    steps: 0
+};
+
+const goal = {
+    x: 31,
+    y: 39
+};
+
+const getType = ({ x, y }: Coords): Type =>
+    (((x * x + 3 * x + 2 * x * y + y + y * y) + favouriteNumber) >>> 0).toString(2).match(/1/g).length & 1;
+
+const serialise = (state: Coords) => `x${state.x}y${state.y}`;
+
+const guessDistance = (position: Coords, goal: Coords) =>
+    Math.sqrt((goal.x - position.x) * (goal.x - position.x) + (goal.y - position.y) * (goal.y - position.y));
+
+const isGoal = (position: Coords, goal: Coords) =>
+    position.x === goal.x && position.y === goal.y;
+
+function* getMoves({ x, y }: Coords) {
+    if (x > 0) {
+        let backwardX = {
+            x: x - 1,
+            y
+        };
+        if (getType(backwardX) === Type.SPACE) yield backwardX;
+    }
+
+    let forwardX = {
+        x: x + 1,
+        y
+    };
+    if (getType(forwardX) === Type.SPACE) yield forwardX;
+
+    if (y > 0) {
+        let backwardY = {
+            x,
+            y: y - 1
+        };
+        if (getType(backwardY) === Type.SPACE) yield backwardY;
+    }
+
+    let forwardY = {
+        x,
+        y: y + 1
+    };
+    if (getType(forwardY) === Type.SPACE) yield forwardY;
+}
+
+const aStar = (start: State, goal: Coords) => {
+    let closedSet = new Set<string>();
+    let serialisedState = serialise(start.coords);
+    let stateLookup = new Map([[serialisedState, start]]);
+
+    let openSet = new Set([serialisedState]);
+    let gScore = new Map([[serialisedState, 0]]);
+    let fScore = new Map([[serialisedState, guessDistance(start.coords, goal)]]);
+
+    while (openSet.size > 0) {
+        let current = Array.from(openSet).sort((a, b) => fScore.get(a) - fScore.get(b))[0];
+        let currentState = stateLookup.get(current);
+        if (isGoal(currentState.coords, goal)) {
+            return currentState.steps;
+        }
+
+        openSet.delete(current);
+        closedSet.add(current);
+        for (let move of getMoves(currentState.coords)) {
+            let serialisedMove = serialise(move);
+            if (serialisedMove in closedSet) {
+                continue;
+            }
+
+            let tentativeGScore = currentState.steps + 1;
+            if (!(serialisedMove in openSet)) {
+                stateLookup.set(serialisedMove, { coords: move, steps: tentativeGScore });
+                openSet.add(serialisedMove);
+            } else if (tentativeGScore >= gScore.get(serialisedMove)) {
+                continue;
+            }
+
+            gScore.set(serialisedMove, tentativeGScore);
+            fScore.set(serialisedMove, gScore[serialisedMove] + guessDistance(move, goal))
+        }
+    }
+};
+
+console.log(aStar(state, goal));
